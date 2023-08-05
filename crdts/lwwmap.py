369eacbd166267c4442ea12c7830f9a024addf0d
Jon Tyler <k98kurz@gmail.com>
2023-08-05 19:32:07 -0400

    moved LWWMap to its own file

---
+++ crdts/lwwmap.py
@@ -0,0 +1,261 @@
+from __future__ import annotations
+from .datawrappers import (
+    BytesWrapper,
+    CTDataWrapper,
+    DecimalWrapper,
+    IntWrapper,
+    NoneWrapper,
+    RGATupleWrapper,
+    StrWrapper,
+)
+from .interfaces import ClockProtocol, DataWrapperProtocol, StateUpdateProtocol
+from .lwwregister import LWWRegister
+from .orset import ORSet
+from .scalarclock import ScalarClock
+from .stateupdate import StateUpdate
+from binascii import crc32
+import json
+import struct
+
+
+class LWWMap:
+    """Implements the Last Writer Wins Map CRDT.
+        https://concordant.gitlabpages.inria.fr/software/c-crdtlib/c-crdtlib/crdtlib.crdt/-l-w-w-map/index.html
+    """
+    names: ORSet
+    registers: dict[DataWrapperProtocol, LWWRegister]
+    clock: ClockProtocol
+
+    def __init__(self, names: ORSet = None, registers: dict = None,
+                clock: ClockProtocol = None
+    ) -> None:
+        """Initialize an LWWMap from an ORSet of names, a list of
+            LWWRegisters, and a shared clock.
+        """
+        assert type(names) is ORSet or names is None, \
+            'names must be an ORSet or None'
+        assert type(registers) is dict or registers is None, \
+            'registers must be a dict mapping names to LWWRegisters or None'
+        assert isinstance(clock, ClockProtocol) or clock is None, \
+            'clock must be a ClockProtocol or None'
+
+        names = ORSet() if names is None else names
+        registers = {} if registers is None else registers
+        clock = ScalarClock() if clock is None else clock
+
+        names.clock = clock
+
+        for name in registers:
+            assert name in names.observed or name in names.removed, \
+                'each register name must be in the names ORSet'
+            assert type(registers[name]) is LWWRegister, \
+                'each element of registers must be an LWWRegister'
+            registers[name].clock = clock
+
+        self.names = names
+        self.registers = registers
+        self.clock = clock
+
+    def pack(self) -> bytes:
+        """Pack the data and metadata into a bytes string."""
+        clock = bytes(bytes(self.clock.__class__.__name__, 'utf-8').hex(), 'utf-8')
+        clock += b'_' + self.clock.pack()
+        names = self.names.pack()
+        registers = {}
+
+        for name in self.names.read():
+            name_class = name.__class__.__name__
+            key = name_class + '_' + name.pack().hex()
+            value_class = self.registers[name].__class__.__name__
+            registers[key] = value_class + '_' + self.registers[name].pack().hex()
+
+        registers = json.dumps(registers, separators=(',', ':'), sort_keys=True)
+        registers = bytes(registers, 'utf-8')
+
+        clock_size = len(clock)
+        names_size = len(names)
+        registers_size = len(registers)
+
+        return struct.pack(
+            f'!III{clock_size}s{names_size}s{registers_size}s',
+            clock_size,
+            names_size,
+            registers_size,
+            clock,
+            names,
+            registers
+        )
+
+    @classmethod
+    def unpack(cls, data: bytes, inject: dict = {}) -> LWWMap:
+        """Unpack the data bytes string into an instance."""
+        assert type(data) is bytes, 'data must be bytes'
+        assert len(data) > 13, 'data must be at least 13 bytes'
+        dependencies = {**globals(), **inject}
+
+        # parse sizes
+        clock_size, names_size, registers_size, _ = struct.unpack(
+            f'!III{len(data)-12}s',
+            data
+        )
+
+        # parse the rest of the data
+        _, _, _, clock, names, registers_raw = struct.unpack(
+            f'!III{clock_size}s{names_size}s{registers_size}s',
+            data
+        )
+
+        # parse the clock and names
+        clock_class, _, clock = clock.partition(b'_')
+        clock_class = str(bytes.fromhex(str(clock_class, 'utf-8')), 'utf-8')
+        assert clock_class in dependencies, f'cannot find {clock_class}'
+        assert hasattr(dependencies[clock_class], 'unpack'), \
+            f'{clock_class} missing unpack method'
+        clock = dependencies[clock_class].unpack(clock)
+        names = ORSet.unpack(names, inject)
+
+        # parse the registers
+        registers_raw = json.loads(str(registers_raw, 'utf-8'))
+        registers = {}
+
+        for key in registers_raw:
+            # resolve key to name
+            name_class, name = key.split('_')
+            name = dependencies[name_class].unpack(bytes.fromhex(name))
+
+            # resolve value
+            value_class, value = registers_raw[key].split('_')
+            value = dependencies[value_class].unpack(bytes.fromhex(value), inject)
+
+            # add to registers
+            registers[name] = value
+
+        return cls(names, registers, clock)
+
+    def read(self) -> dict:
+        """Return the eventually consistent data view."""
+        result = {}
+
+        for name in self.names.read():
+            result[name] = self.registers[name].read()
+
+        return result
+
+    def update(self, state_update: StateUpdateProtocol) -> LWWMap:
+        """Apply an update and return self (monad pattern)."""
+        assert isinstance(state_update, StateUpdateProtocol), \
+            'state_update must be instance implementing StateUpdateProtocol'
+        assert state_update.clock_uuid == self.clock.uuid, \
+            'state_update.clock_uuid must equal CRDT.clock.uuid'
+        assert type(state_update.data) is tuple, \
+            'state_update.data must be tuple of (str, DataWrapperProtocol, int, DataWrapperProtocol)'
+        assert len(state_update.data) == 4, \
+            'state_update.data must be tuple of (str, DataWrapperProtocol, int, DataWrapperProtocol)'
+
+        op, name, writer, value = state_update.data
+        assert type(op) is str and op in ('o', 'r'), \
+            'state_update.data[0] must be str op one of (\'o\', \'r\')'
+        assert isinstance(name, DataWrapperProtocol), \
+            'state_update.data[1] must be DataWrapperProtocol name'
+        assert type(writer) is int, \
+            'state_update.data[2] must be int writer id'
+        assert isinstance(value, DataWrapperProtocol), \
+            'state_update.data[3] must be DataWrapperProtocol value'
+
+        ts = state_update.ts
+
+        if op == 'o':
+            # try to add to the names ORSet
+            self.names.update(StateUpdate(self.clock.uuid, ts, ('o', name)))
+
+            # if register missing and name added successfully, create register
+            if name not in self.registers and name in self.names.read():
+                self.registers[name] = LWWRegister(name, value, self.clock, ts, writer)
+
+        if op == 'r':
+            # try to remove from the names ORSet
+            self.names.update(StateUpdate(self.clock.uuid, ts, ('r', name)))
+
+        # if the register exists, update it
+        if name in self.registers:
+            self.registers[name].update(StateUpdate(self.clock.uuid, ts, (writer, value)))
+
+        return self
+
+    def checksums(self) -> tuple[int]:
+        """Returns any checksums for the underlying data to detect
+            desynchronization due to message failure.
+        """
+        names_checksums = self.names.checksums()
+        total_last_update = 0
+        total_last_writer = 0
+        total_register_crc32 = 0
+
+        for name in self.names.read():
+            total_register_crc32 += crc32(self.registers[name].pack())
+            total_last_update += self.registers[name].last_update
+            total_last_writer += self.registers[name].last_writer
+
+        return (
+            total_last_update % 2**32,
+            total_last_writer % 2**32,
+            total_register_crc32 % 2**32,
+            *names_checksums
+        )
+
+    def history(self) -> tuple[StateUpdate]:
+        """Returns a concise history of StateUpdates that will converge
+            to the underlying data. Useful for resynchronization by
+            replaying all updates from divergent nodes.
+        """
+        registers_history = {}
+        orset_history = self.names.history()
+        history = []
+
+        for name in self.registers:
+            registers_history[name] = self.registers[name].history()
+
+        for update in orset_history:
+            name = update.data[1]
+            if name in registers_history:
+                register_update = registers_history[name][0]
+                history.append(StateUpdate(
+                    update.clock_uuid,
+                    register_update.ts,
+                    (update.data[0], name, register_update.data[0], register_update.data[1])
+                ))
+
+        return tuple(history)
+
+    def extend(self, name: DataWrapperProtocol, value: DataWrapperProtocol,
+                writer: int) -> StateUpdate:
+        """Extends the dict with name: value. Returns a StateUpdate."""
+        assert isinstance(name, DataWrapperProtocol), \
+            'name must be a DataWrapperProtocol'
+        assert isinstance(value, DataWrapperProtocol) or value is None, \
+            'value must be a DataWrapperProtocol or None'
+        assert type(writer) is int, 'writer must be an int'
+
+        state_update = StateUpdate(
+            self.clock.uuid,
+            self.clock.read(),
+            ('o', name, writer, value)
+        )
+        self.update(state_update)
+
+        return state_update
+
+    def unset(self, name: DataWrapperProtocol, writer: int) -> StateUpdate:
+        """Removes the key name from the dict. Returns a StateUpdate."""
+        assert isinstance(name, DataWrapperProtocol), \
+            'name must be a DataWrapperProtocol'
+        assert type(writer) is int, 'writer must be an int'
+
+        state_update = StateUpdate(
+            self.clock.uuid,
+            self.clock.read(),
+            ('r', name, writer, NoneWrapper())
+        )
+        self.update(state_update)
+
+        return state_update
